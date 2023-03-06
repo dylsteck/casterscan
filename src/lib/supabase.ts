d514f611c52a19e407ddd517fa380e64cc98bb60
Dylan Steck <dylancsteck@gmail.com>
2023-03-05 21:42:51 -0500

    Shows count and starts to lay out other stats for homepage

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL ?? '';
-const supabaseKey = process.env.SUPABASE_KEY ?? '';
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = process.env.SUPABASE_URL ?? '';
+const supabaseUrl = 'https://jzdvwdhdnueugpdzrsxb.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY ?? '';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6ZHZ3ZGhkbnVldWdwZHpyc3hiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3ODA2MDA5OSwiZXhwIjoxOTkzNjM2MDk5fQ.tzpkcmX_9ztH25_PyZV2PVxeRbFafH9fmuGpaqeZvcs'
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,49 @@
 import Layout from "@/components/Layout"
+import { supabase } from "@/lib/supabase"
+import { DocumentDuplicateIcon } from "@heroicons/react/24/solid"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [totalCasts, setTotalCasts] = useState(0)
+
+  useEffect(() => {
+    async function check(){
+      const { count } = await supabase.from('casts').select('*', {count: 'exact'})
+      setTotalCasts(count)
+    }
+    check()
+  }, [totalCasts])
+
   return (
     <Layout>
-      {/* TODO: Set up Analytics & Casts/Users List */}
+      <div className="rounded-lg bg-white p-3 w-[50vw] h-[25vh] mx-auto mt-[25vh]">
+        <div className="flex flex-row">
+          <div className="ml-2 mt-2">
+            <DocumentDuplicateIcon className="w-6 h-6 inline-block" />
+            <h3 className="font-medium text-2xl inline-block ml-2">Total Casts</h3>
+            <h3 className="font-regular text-lg ml-10">{totalCasts}</h3>
+          </div>
+          <div className="ml-5 mt-2">
+            <DocumentDuplicateIcon className="w-6 h-6 inline-block" />
+            <h3 className="font-medium text-2xl inline-block ml-2">Avg. Casts Per Day</h3>
+            <h3 className="font-regular text-lg ml-10">2343</h3>
+          </div>
+        </div>
+        <hr className="mt-3 mb-1" />
+        <div className="flex flex-row mt-2">
+          <div className="ml-2 mt-2">
+            <DocumentDuplicateIcon className="w-6 h-6 inline-block" />
+            <h3 className="font-medium text-2xl inline-block ml-2">Buying Power</h3>
+            <h3 className="font-regular text-lg ml-10">{totalCasts}</h3>
+          </div>
+          <div className="ml-5 mt-2">
+            <DocumentDuplicateIcon className="w-6 h-6 inline-block" />
+            <h3 className="font-medium text-2xl inline-block ml-2">Last Cast</h3>
+            <h3 className="font-regular text-lg ml-10">{totalCasts}</h3>
+          </div>
+        </div>
+        {/* TODO: Insert graph here & turn flex children into component */}
+      </div>
     </Layout>
   )
 }
