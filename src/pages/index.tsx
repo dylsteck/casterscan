8f471fd7934c200921b1dd26c1379b9ad4c85c8c
Dylan Steck <12853808+dylsteck@users.noreply.github.com>
2023-09-01 19:25:14 +0000

    fix google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 import { useContext } from "react";
 import Header from "~/components/Header";
 import LiveFeed from "~/components/LiveFeed";
@@ -17,6 +18,16 @@ const Home: NextPage = () => {
         <meta name="description" content="A block explorer for the Farcaster network" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-1G0WTCHYKQ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1G0WTCHYKQ');
+        `}
+      </Script>  {/* TODO: see if can remove the other Google analytics package now */}
       { searchValue.length > 0 ? <Search /> : <LiveFeed /> }
     </>
   );
