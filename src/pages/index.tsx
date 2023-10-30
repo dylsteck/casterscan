2630f21dc5bb6c9a417376576f046fad4bc8b951
dylsteck <dylancsteck@gmail.com>
2023-10-30 13:43:56 -0700

    Change navbar color to white for the pwa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,11 @@ const Home: NextPage = () => {
       <Head>
         <title>Casterscan</title>
         <meta name="description" content="A block explorer for Farcaster" />
+        {/* these theme-color and apple-mobile-web meta tags set the theme and navbar colors for the pwa */}
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-B02X48B2NQ" />
