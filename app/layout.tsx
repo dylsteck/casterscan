d2a85adf968ed493bb6565ac6e4e320c8bcdee49
dylsteck <dylancsteck@gmail.com>
2025-01-03 22:35:22 -0500

    remove vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 import Header from "./components/header";
 import Script from "next/script";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { BANNER_IMG_URL, frame, ICON_IMG_URL } from "./lib/utils";
 import FrameProvider from "./components/frame-provider";
 
@@ -58,7 +57,6 @@ export default function RootLayout({
         <Header />
         <FrameProvider>
           {children}
-          <SpeedInsights />
         </FrameProvider>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ""}`} />
         <Script id="google-analytics">
