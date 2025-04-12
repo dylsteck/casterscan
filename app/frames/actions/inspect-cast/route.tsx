59aae1cbf571ec5567bc41a5af4d675ab926b11b
dylsteck <dylancsteck@gmail.com>
2025-04-12 18:56:28 -0400

    remove cast action mini app deeplinks while investigating why their params wont work

--- app/frames/actions/inspect-cast/route.tsx
+++ app/frames/actions/inspect-cast/route.tsx
@@ -19,10 +19,9 @@ export const GET = async (req: NextRequest) => {
 
 export const POST = frames(async (ctx) => {
   const hash = ctx.message?.castId?.hash;
-  const miniAppUrl = `https://warpcast.com/~/frames/launch?domain=${encodeURIComponent(`${SEO.url}/casts/${hash}`)}`;
   return Response.json({
     message: `Click to Inspect Cast`,
-    link: miniAppUrl,
+    link: `${SEO.url}/casts/${hash}`,
     type: 'message'
   });
 });
--- app/frames/cast/route.tsx
+++ app/frames/cast/route.tsx
@@ -6,14 +6,13 @@ import { SEO } from "@/app/lib/utils";
 
 export const GET = frames(async (ctx) => {
     const hash = ctx.searchParams.hash;
-    const miniAppUrl = `https://warpcast.com/~/frames/launch?domain=${encodeURIComponent(`${SEO.url}/casts/${hash}`)}`;
     return {
       image: `https://client.warpcast.com/v2/cast-image?castHash=${hash}`,
       imageOptions: {
         aspectRatio: '1:1'
       },
       buttons: [
-        <Button action="link" target={miniAppUrl}>
+        <Button action="link" target={`${SEO.url}/casts/${hash}`}>
           View on Casterscan
         </Button>,
         <Button action="link" target="https://warpcast.com/~/add-cast-action?url=https://casterscan.com/frames/actions/inspect-cast">
